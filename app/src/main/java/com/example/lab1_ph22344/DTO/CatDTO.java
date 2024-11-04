0a29f759f336e1c19a15ba82fd0396de1e6ece1f
Light <danglhph22344@fpt.edu.vn>
2024-11-04 17:17:40 +0700

    Tạo file kết nối CSDL

--- app/src/main/java/com/example/lab1_ph22344/DTO/CatDTO.java
+++ app/src/main/java/com/example/lab1_ph22344/DTO/CatDTO.java
@@ -1,4 +1,28 @@
 package com.example.lab1_ph22344.DTO;
 
 public class CatDTO {
+    private int id;
+    private String name;
+
+    public CatDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getter và Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/example/lab1_ph22344/DbHelper/MyDbHelper.java
+++ app/src/main/java/com/example/lab1_ph22344/DbHelper/MyDbHelper.java
@@ -1,4 +1,29 @@
 package com.example.lab1_ph22344.DbHelper;
 
-public class MyDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "SalesManagement.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createCatTable = "CREATE TABLE tb_cat (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)";
+        String createProductTable = "CREATE TABLE tb_product (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, id_cat INTEGER, FOREIGN KEY (id_cat) REFERENCES tb_cat(id))";
+        db.execSQL(createCatTable);
+        db.execSQL(createProductTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS tb_cat");
+        db.execSQL("DROP TABLE IF EXISTS tb_product");
+        onCreate(db);
+    }
 }
